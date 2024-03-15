5e61e05dd00eecedd819ba7d4201a83e244501c1
dtbooy <dtbooy@gmail.com>
2024-03-15 10:09:52 +1000

    setup DB connection

---
+++ controllers/db.js
@@ -0,0 +1,18 @@
+import { Sequelize, DataTypes } from 'sequelize'
+import dotenv from 'dotenv'
+
+// config method reads env file and sets up envioronment variables
+dotenv.config()
+
+// database connection
+const sequelize = new Sequelize(process.env.DB_URI)
+
+// test db connection works
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+
+export { sequelize }
